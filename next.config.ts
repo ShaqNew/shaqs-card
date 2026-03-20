547286d57fdf23e3544991a9c22f70e175bc694d
ShaqNew <shaquillenewell@gmail.com>
2026-03-20 20:55:50 +0000

    Added page for fetching trading data from droplet

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/trading/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_TRADING_API_HOST}:8080/:path*`, 
+      },
+    ]
+  },
 };
 
 export default nextConfig;
